eef6bd2324f539c57c8d31a222c29de15efdf57d
Łukasz Kilijański <lukasz@ukaszs-Mac-mini.local>
2023-06-05 18:14:56 +0200

    9 - Change vendor name.

--- registration.php
+++ registration.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Solid_Blog', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'STLK_Blog', __DIR__);
